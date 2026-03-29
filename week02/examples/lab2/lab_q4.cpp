5300257d0ba1f0e8196567e0bec35c0b9eeeec38
Magiz0r <fomag924@gmail.com>
2026-03-29 15:57:15 -0700

    .

---
+++ week02/examples/lab2/lab_q4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    double c, d,f,g;
+    char h;
+
+    a = 19.99 + 21.99;
+    b = (int)19.99 + 21.99;
+    c = 23 / 3;
+    d = 23 / 3.0;
+    f = 23 / 3.0e4;
+    g = 23 / 3.0e5;
+    h = 'b' - 32;
+
+    //complete code here
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+    return 0;
+}
